5620bd3eca37f99d96574eb0002d17d466b3ebd0
learning-ops-git <higuitadaniel94@gmail.com>
2025-10-29 12:21:04 -0500

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "jobs" {
     aws.main = aws.main
   }
 
-  source                     = "../mod-glue-job"
+  source                     = "./mod-glue-job"
   for_each                   = local.glue_jobs_definition
   country                    = var.country
   env                        = var.env
