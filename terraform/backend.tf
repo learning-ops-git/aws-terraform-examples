9aedeac544a39c548315b32f28c8a4b4bb80691b
learning-ops-git <higuitadaniel94@gmail.com>
2025-11-15 21:13:03 -0500

    updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
-  cloud {
-    organization = "ProjectsTerraform2025"
+  backend "s3" {
+    bucket = "co-data-engineering-216461881627-dev-artifacts"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
 
-    workspaces {
-      name = "lambda-module-dev"
-    }
+    # Optional: Encrypt state file
+    encrypt = true
   }
 }
